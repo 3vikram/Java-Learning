f6c967b284148d0fe96f44a797962c7ffd4f1364
3vikram <viku.vikram05@gmail.com>
2019-02-27 19:29:21 +0530

    Update arraylist.java

--- Data Structures/Array List/arraylist.java
+++ Data Structures/Array List/arraylist.java
@@ -5,10 +5,10 @@ public class Temperatures {
 	public static void main(String[] args) {
 		ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>();
     
-    weeklyTemperatures.add(25);
-    weeklyTemperatures.add(25);
+    		weeklyTemperatures.add(25);
+    		weeklyTemperatures.add(25);
     
-    System.out.println(weeklyTemperatures);
+    		System.out.println(weeklyTemperatures);
 
 	}
 }
