7f50c67b8131eb9a6fb12b9267526d41fb200c4a
3vikram <viku.vikram05@gmail.com>
2019-02-27 16:13:32 +0530

    Update constructor.java

--- Constructor/constructor.java
+++ Constructor/constructor.java
@@ -2,14 +2,18 @@ class Dog {
 
 	int age;
 	
+	// Constructor of Dog class below takes dogs age as a argument
 	public Dog(int dogsAge) {
     		age = dogsAge;
   	}
   
+	// bark method which prints a statement
+	
 	public void bark() {
     		System.out.println("Woof!");
   	}
-
+	
+	// main method, here an instance of Dog class is created and this instance object is named as 'spike'
 	public static void main(String[] args) {
     
     		Dog spike = new Dog(5);
