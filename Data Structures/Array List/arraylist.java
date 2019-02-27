0798e436a142d09b668fd73842c2b3560345e9d2
3vikram <viku.vikram05@gmail.com>
2019-02-27 19:28:19 +0530

    Create arraylist.java

---
+++ Data Structures/Array List/arraylist.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Temperatures {
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>();
+    
+    weeklyTemperatures.add(25);
+    weeklyTemperatures.add(25);
+    
+    System.out.println(weeklyTemperatures);
+
+	}
+}
