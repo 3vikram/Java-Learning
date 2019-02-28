3f4b735931449b6abbeae057143bad1167bdb9f6
3vikram <viku.vikram05@gmail.com>
2019-02-28 21:32:50 +0530

    Create childclass.java

---
+++ Method Overriding/Example 3/childclass.java
@@ -0,0 +1,9 @@
+class MyChildClass extends MyBaseClass{
+   public void disp(){
+      System.out.println("Child class method");
+   }
+   public static void main( String args[]) {
+      MyChildClass obj = new MyChildClass();
+      obj.disp();
+   }
+}
