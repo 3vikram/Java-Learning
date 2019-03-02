9c0c7e5a4a39c4f0c6420dd94c9a45f6c7dd2224
3vikram <viku.vikram05@gmail.com>
2019-03-02 20:40:13 +0530

    Create overloadingstatic-nonstatic.java

---
+++ Method Overloading/overloadingstatic-nonstatic.java
@@ -0,0 +1,38 @@
+package com.training.class2;
+
+public class Lion implements Animal {
+	
+	public static void test1() {
+		System.out.println("running test");
+	}
+	
+	public void test1(int a) {
+		System.out.println(a); 
+	}
+	
+	public void eat() {
+		System.out.println("Deer");
+	}
+	
+	public void home() {
+		System.out.println("Nairobi, Africa");
+	}
+	
+	public int sleep() {
+		return 18;
+	}
+	
+	public static void main(String args[]) {
+		
+		Lion africanLion = new Lion();
+		africanLion.eat();
+		africanLion.home();
+		System.out.println(africanLion.sleep());
+		System.out.println(africanLion.getClass());
+		System.out.println(africanLion.category);
+		africanLion.test1(25);
+		Lion.test1();
+		
+	}
+
+}
