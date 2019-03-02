686db0a9be4ebe1317310905f76e75269ad90646
3vikram <viku.vikram05@gmail.com>
2019-03-02 18:57:17 +0530

    Create Animal.java

---
+++ Interface/Animal.java
@@ -0,0 +1,13 @@
+package com.training.class2;
+
+public interface Animal {
+	
+	public final String category = "Carnivorous";
+	
+	public void eat();
+	
+	public void home();
+	
+	public int sleep();
+
+}
