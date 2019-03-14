b0ecb13bcf6815102c5f37c796b4759dba686bd3
3vikram <viku.vikram05@gmail.com>
2019-03-14 21:05:44 +0530

    Create CollectionArrayList.java

---
+++ Collections/List/ArrayList/CollectionArrayList.java
@@ -0,0 +1,30 @@
+package com.java.practice;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Collections {
+
+	public static void main(String[] args) {
+		
+		Collection values = new ArrayList();
+		values.add(4);
+		values.add("test");
+		values.add(true);
+		values.add('C');
+		values.add(2.50);
+		
+		for (Object value: values) {
+			System.out.println(value);
+		}
+	}
+}
+
+
+Result:
+
+4
+test
+true
+C
+2.5
