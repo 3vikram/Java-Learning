82fd7fe7c98d6a4d9c0c0727be2a43d4c1e95e9a
3vikram <viku.vikram05@gmail.com>
2019-03-14 23:02:18 +0530

    Update hashmap.java

--- Collections/Map/HashMap/hashmap.java
+++ Collections/Map/HashMap/hashmap.java
@@ -2,6 +2,27 @@ import java.util.HashMap;
 
 public class RestaurantForEach {
 	public static void main(String[] args) {
+		
+		/*
+		    hostCities.put("Beijing", 2008);
+		    hostCities.put("London", 2012);
+		    hostCities.put("Rio de Janeiro", 2016);
+
+		    hostCities.keySet()  // returns keys
+		    hostCities.values() // returns values
+		    hostCities.get("Beijing")
+		    hostCities.size()
+		    hostCities.containsKey("London")
+		    hostCities.isEmpty()
+		    hostCities.clear()
+		    hostCities.containsKey("London")
+		    hostCities.containsValue(2016)
+		    hostCities.clone()
+		    newhostCities.putAll(hostCities)
+		    hostCities.remove("Beijing")
+    
+		    HashMap doesn’t allow duplicate keys but allows duplicate values.
+		*/
 
 		HashMap<String, Integer> restaurantMenu = new HashMap<String, Integer>();
 
