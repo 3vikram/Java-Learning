f2413b70d5fc7e88d4754145aa566220c7ac13c2
3vikram <viku.vikram05@gmail.com>
2019-03-14 23:01:19 +0530

    Update ArrayList.java

--- Collections/List/ArrayList/ArrayList.java
+++ Collections/List/ArrayList/ArrayList.java
@@ -3,6 +3,20 @@ import java.util.*;
 public class Temperatures {
 	
 	public static void main(String[] args) {
+		
+		/*
+		 	olympicSports.remove(3)
+    			olympicSports.size()
+    			olympicSports.contains("Cricket")
+    			olympicSports.get(1)
+			olympicSports.subList(1,3) // returns the items in the Arraylist from index 1 till 2
+			olympicSports.set(2, "Baseball") // replaces the index 2 value to "Baseball"
+			olympicSports.removeAll()
+			olympicSports.isEmpty()
+			olympicSports.removeRange(0, 2)  // removes elements in the index from 0 till 1
+			olympicSports.clear()
+			olympicSports.removeAll(arrlist2); // removes the arrlist2 elements present in olympicSports
+		*/
 		ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>();
     
     		weeklyTemperatures.add(78);
