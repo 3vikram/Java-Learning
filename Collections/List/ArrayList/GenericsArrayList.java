4309036c8bb1c4a188affba1172db70b669f0ca2
3vikram <viku.vikram05@gmail.com>
2019-03-14 21:20:20 +0530

    Update GenericsArrayList.java

--- Collections/List/ArrayList/GenericsArrayList.java
+++ Collections/List/ArrayList/GenericsArrayList.java
@@ -8,6 +8,7 @@ public class Collections {
 	public static void main(String[] args) {
 		
 		//Below line defines Generics as we use Integer type with the collection values
+		//Collection is an interface which has access to limited methods on an element, ArrayList is an implementation or Class
 		Collection<Integer> values = new ArrayList<Integer>();
 		values.add(4);
 		values.add(16);
@@ -15,7 +16,7 @@ public class Collections {
 		values.add(9);
 		values.add(2);
 		
-		for (Object value: values) {
+		for (Integer value: values) {
 			System.out.println(value);
 		}
 	}
