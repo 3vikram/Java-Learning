def59de2a2a469edc6e9520f0f0acba898dfaf5c
3vikram <viku.vikram05@gmail.com>
2019-03-19 17:49:46 +0530

    Update HashSet.java

--- Collections/Set/HashSet.java
+++ Collections/Set/HashSet.java
@@ -8,6 +8,7 @@ public class Collections {
 	public static void main(String[] args) {
 		
 		//Set is the Interface, HashSet is the implementation, Set allows only unique values and no duplicate values
+		// The sequence of elements added are not maintained as it uses hashing to compute the nearest values it can retrieve
 		
 		Set<Integer> values = new HashSet<>();
 		
