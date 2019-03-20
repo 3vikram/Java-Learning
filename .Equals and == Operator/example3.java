9861f3981fae442ce6542f477c325f62f36ccfa3
3vikram <viku.vikram05@gmail.com>
2019-03-20 08:39:08 +0530

    Update example3.java

--- .Equals and == Operator/example3.java
+++ .Equals and == Operator/example3.java
@@ -15,6 +15,7 @@ public class Collections {
 		System.out.println(s2.hashCode());
 		System.out.println(s1.equals(s2)); // .equals() method compares the contents of 2 objects
 		//Note: .equals() method can be overriden to compare at reference level as well based on our requirement
+		//In String class, all wrapper class and collections class .equals() method is overriden to compare at content level
 	}
 }
 
