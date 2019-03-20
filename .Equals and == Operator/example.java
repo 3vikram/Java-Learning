e4a65d386880c65c1760a84c8bf91e5caa98f5db
3vikram <viku.vikram05@gmail.com>
2019-03-20 08:32:58 +0530

    Update example.java

--- .Equals and == Operator/example.java
+++ .Equals and == Operator/example.java
@@ -5,7 +5,7 @@ public class Collections {
 	public static void main(String[] args) {
 		
 		String s1 = "test"; 
-		String s2 = "test"; // points to the same reference as s1 as the content is same
+		String s2 = "test"; // points to the same reference as s1 as the content is same (String literals are used)
 		
 		System.out.println(s1.hashCode());
 		System.out.println(s2.hashCode());
