0ab7f37e31a231e535c18834a1159fb5df134afa
3vikram <viku.vikram05@gmail.com>
2019-03-20 10:59:26 +0530

    Create ToString.java

---
+++ Hash Code/ToString.java
@@ -0,0 +1,16 @@
+package com.java.practice;
+
+public class Collections {
+	
+
+	public static void main(String[] args) {
+		
+		Collections c = new Collections();
+		System.out.println(c.toString()); // .toString() method returns classname@HashCode(Converted from int to Hex)
+	}
+}
+
+
+Result:
+
+com.java.practice.Collections@70dea4e
