5f21acb5c5b146d42836ddbb41cd01e3d43fa7ac
3vikram <viku.vikram05@gmail.com>
2019-03-20 08:34:30 +0530

    Create example3.java

---
+++ .Equals and == Operator/example3.java
@@ -0,0 +1,24 @@
+package com.java.practice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Collections {
+
+	public static void main(String[] args) {
+		
+		String s1 = new String("test"); // Creates a new Object of type string in heap memory
+		String s2 = new String("test");
+		
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+		System.out.println(s1.equals(s2));
+	}
+}
+
+Result:
+
+3556498
+3556498
+true
