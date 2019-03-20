b3612599f3e0a0ae481d24514e56dff356f750fc
3vikram <viku.vikram05@gmail.com>
2019-03-20 12:14:55 +0530

    Create ToString2.java

---
+++ Hash Code/ToString2.java
@@ -0,0 +1,16 @@
+package com.java.practice;
+
+public class Collections {
+	
+
+	public static void main(String[] args) {
+		
+		Collections c = new Collections();
+		System.out.println(c);
+	}
+}
+
+
+Result:
+
+com.java.practice.Collections@70dea4e
