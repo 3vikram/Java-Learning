2102fa0d442fa3146a84cf06bb6228b5a4fc340d
3vikram <viku.vikram05@gmail.com>
2019-03-20 08:35:06 +0530

    Update example3.java

--- .Equals and == Operator/example3.java
+++ .Equals and == Operator/example3.java
@@ -13,7 +13,7 @@ public class Collections {
 		
 		System.out.println(s1.hashCode());
 		System.out.println(s2.hashCode());
-		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals(s2)); // .equals() method compares the contents of 2 objects
 	}
 }
 
