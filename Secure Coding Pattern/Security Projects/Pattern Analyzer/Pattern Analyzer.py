da44d4b5ff27b84a45d39b9018c5309a928c3ff4
3vikram <viku.vikram05@gmail.com>
2019-08-12 13:24:51 +0530

    Create Pattern Analyzer.py

---
+++ Secure Coding Pattern/Security Projects/Pattern Analyzer/Pattern Analyzer.py
@@ -0,0 +1,17 @@
+import re
+
+def regex_pattern_validation(pattern, file_path):
+
+    compiled_pattern = re.compile(pattern)
+    identified_pattern = []
+
+    for i, line in enumerate(open(file_path)):
+        for match in re.finditer(compiled_pattern, line):
+            identified_pattern.append(match.group())
+
+    return identified_pattern
+
+if __name__ == "__main__":
+    pattern = input("Enter the pattern: ")
+    file_path = input("Enter the file name and path: ")
+    print(regex_pattern_validation(pattern, file_path))
